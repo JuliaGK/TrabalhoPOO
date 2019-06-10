8b857c3b1b75d90acfebad0820c2c7b586f730d5
Julia <ju.kuchle@gmail.com>
2019-06-10 13:47:32 -0300

    Modelo

--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -1,5 +1,289 @@
 package modelo;
 
-public class Modelo {
-
+public class CadastroEstudante {
+	private String cpfEstudante;
+	private String nomeEstudante;
+	private String rgEstudante;
+	private String enderecoEtudante;
+	private String bairroEstudante;
+	private String cidadeEstudante;
+	private String cursoEstudante; //JComboBox
+	private String semestEstudante; //JComboBox
+	private String dataNascEstudante; 
+	private String complemEstudante;
+	private String cepEstudante;
+	private String ufEstudante; //JComboBox
+	private String matricEstudante;
+	
+	private String nomeCoord;
+	private String cpfCoord;
+	private String rgCoord;
+	private String cursoCoord; //JComboBox
+	
+	private String nomeResp;
+	private String cpfResp;
+	private String rgResp;
+	private String cargoResp;
+	
+	private String areaAtuacaoEstagio;
+	private String dataInicialEstagio;
+	private String cargaHorariaEstagio;
+	private String numApoliceSeguroEstagio;
+	private String duracaoEstagio;
+	private String dataFinalEstagio;
+	private String seguradoraEstagio;
+	private String numSiapeEstagio;
+	private String cargoSupervEstagio;
+	private String atividadesEstagio;
+	
+	//PanelLogin
+	private String usuarioLogin;
+	private String senhaLogin;
+	
+	
+	public CadastroEstudante(String cpfEstudante, String nomeEstudante, String rgEstudante, String enderecoEtudante,
+			String bairroEstudante, String cidadeEstudante, String cursoEstudante, String semestEstudante,
+			String dataNascEstudante, String complemEstudante, String cepEstudante, String ufEstudante,
+			String matricEstudante, String nomeCoord, String cpfCoord, String rgCoord, String cursoCoord,
+			String nomeResp, String cpfResp, String rgResp, String cargoResp, String areaAtuacaoEstagio,
+			String dataInicialEstagio, String cargaHorariaEstagio, String numApoliceSeguroEstagio,
+			String duracaoEstagio, String dataFinalEstagio, String seguradoraEstagio, String numSiapeEstagio,
+			String cargoSupervEstagio, String atividadesEstagio, String usuarioLogin, String senhaLogin) {
+		
+		super();
+		this.cpfEstudante = cpfEstudante;
+		this.nomeEstudante = nomeEstudante;
+		this.rgEstudante = rgEstudante;
+		this.enderecoEtudante = enderecoEtudante;
+		this.bairroEstudante = bairroEstudante;
+		this.cidadeEstudante = cidadeEstudante;
+		this.cursoEstudante = cursoEstudante;
+		this.semestEstudante = semestEstudante;
+		this.dataNascEstudante = dataNascEstudante;
+		this.complemEstudante = complemEstudante;
+		this.cepEstudante = cepEstudante;
+		this.ufEstudante = ufEstudante;
+		this.matricEstudante = matricEstudante;
+		this.nomeCoord = nomeCoord;
+		this.cpfCoord = cpfCoord;
+		this.rgCoord = rgCoord;
+		this.cursoCoord = cursoCoord;
+		this.nomeResp = nomeResp;
+		this.cpfResp = cpfResp;
+		this.rgResp = rgResp;
+		this.cargoResp = cargoResp;
+		this.areaAtuacaoEstagio = areaAtuacaoEstagio;
+		this.dataInicialEstagio = dataInicialEstagio;
+		this.cargaHorariaEstagio = cargaHorariaEstagio;
+		this.numApoliceSeguroEstagio = numApoliceSeguroEstagio;
+		this.duracaoEstagio = duracaoEstagio;
+		this.dataFinalEstagio = dataFinalEstagio;
+		this.seguradoraEstagio = seguradoraEstagio;
+		this.numSiapeEstagio = numSiapeEstagio;
+		this.cargoSupervEstagio = cargoSupervEstagio;
+		this.atividadesEstagio = atividadesEstagio;
+		this.usuarioLogin = usuarioLogin;
+		this.senhaLogin = senhaLogin;
+	}
+	
+	
+	public String getCpfEstudante() {
+		return cpfEstudante;
+	}
+	public void setCpfEstudante(String cpfEstudante) {
+		this.cpfEstudante = cpfEstudante;
+	}
+	public String getNomeEstudante() {
+		return nomeEstudante;
+	}
+	public void setNomeEstudante(String nomeEstudante) {
+		this.nomeEstudante = nomeEstudante;
+	}
+	public String getRgEstudante() {
+		return rgEstudante;
+	}
+	public void setRgEstudante(String rgEstudante) {
+		this.rgEstudante = rgEstudante;
+	}
+	public String getEnderecoEtudante() {
+		return enderecoEtudante;
+	}
+	public void setEnderecoEtudante(String enderecoEtudante) {
+		this.enderecoEtudante = enderecoEtudante;
+	}
+	public String getBairroEstudante() {
+		return bairroEstudante;
+	}
+	public void setBairroEstudante(String bairroEstudante) {
+		this.bairroEstudante = bairroEstudante;
+	}
+	public String getCidadeEstudante() {
+		return cidadeEstudante;
+	}
+	public void setCidadeEstudante(String cidadeEstudante) {
+		this.cidadeEstudante = cidadeEstudante;
+	}
+	public String getCursoEstudante() {
+		return cursoEstudante;
+	}
+	public void setCursoEstudante(String cursoEstudante) {
+		this.cursoEstudante = cursoEstudante;
+	}
+	public String getSemestEstudante() {
+		return semestEstudante;
+	}
+	public void setSemestEstudante(String semestEstudante) {
+		this.semestEstudante = semestEstudante;
+	}
+	public String getDataNascEstudante() {
+		return dataNascEstudante;
+	}
+	public void setDataNascEstudante(String dataNascEstudante) {
+		this.dataNascEstudante = dataNascEstudante;
+	}
+	public String getComplemEstudante() {
+		return complemEstudante;
+	}
+	public void setComplemEstudante(String complemEstudante) {
+		this.complemEstudante = complemEstudante;
+	}
+	public String getCepEstudante() {
+		return cepEstudante;
+	}
+	public void setCepEstudante(String cepEstudante) {
+		this.cepEstudante = cepEstudante;
+	}
+	public String getUfEstudante() {
+		return ufEstudante;
+	}
+	public void setUfEstudante(String ufEstudante) {
+		this.ufEstudante = ufEstudante;
+	}
+	public String getMatricEstudante() {
+		return matricEstudante;
+	}
+	public void setMatricEstudante(String matricEstudante) {
+		this.matricEstudante = matricEstudante;
+	}
+	public String getNomeCoord() {
+		return nomeCoord;
+	}
+	public void setNomeCoord(String nomeCoord) {
+		this.nomeCoord = nomeCoord;
+	}
+	public String getCpfCoord() {
+		return cpfCoord;
+	}
+	public void setCpfCoord(String cpfCoord) {
+		this.cpfCoord = cpfCoord;
+	}
+	public String getRgCoord() {
+		return rgCoord;
+	}
+	public void setRgCoord(String rgCoord) {
+		this.rgCoord = rgCoord;
+	}
+	public String getCursoCoord() {
+		return cursoCoord;
+	}
+	public void setCursoCoord(String cursoCoord) {
+		this.cursoCoord = cursoCoord;
+	}
+	public String getNomeResp() {
+		return nomeResp;
+	}
+	public void setNomeResp(String nomeResp) {
+		this.nomeResp = nomeResp;
+	}
+	public String getCpfResp() {
+		return cpfResp;
+	}
+	public void setCpfResp(String cpfResp) {
+		this.cpfResp = cpfResp;
+	}
+	public String getRgResp() {
+		return rgResp;
+	}
+	public void setRgResp(String rgResp) {
+		this.rgResp = rgResp;
+	}
+	public String getCargoResp() {
+		return cargoResp;
+	}
+	public void setCargoResp(String cargoResp) {
+		this.cargoResp = cargoResp;
+	}
+	public String getAreaAtuacaoEstagio() {
+		return areaAtuacaoEstagio;
+	}
+	public void setAreaAtuacaoEstagio(String areaAtuacaoEstagio) {
+		this.areaAtuacaoEstagio = areaAtuacaoEstagio;
+	}
+	public String getDataInicialEstagio() {
+		return dataInicialEstagio;
+	}
+	public void setDataInicialEstagio(String dataInicialEstagio) {
+		this.dataInicialEstagio = dataInicialEstagio;
+	}
+	public String getCargaHorariaEstagio() {
+		return cargaHorariaEstagio;
+	}
+	public void setCargaHorariaEstagio(String cargaHorariaEstagio) {
+		this.cargaHorariaEstagio = cargaHorariaEstagio;
+	}
+	public String getNumApoliceSeguroEstagio() {
+		return numApoliceSeguroEstagio;
+	}
+	public void setNumApoliceSeguroEstagio(String numApoliceSeguroEstagio) {
+		this.numApoliceSeguroEstagio = numApoliceSeguroEstagio;
+	}
+	public String getDuracaoEstagio() {
+		return duracaoEstagio;
+	}
+	public void setDuracaoEstagio(String duracaoEstagio) {
+		this.duracaoEstagio = duracaoEstagio;
+	}
+	public String getDataFinalEstagio() {
+		return dataFinalEstagio;
+	}
+	public void setDataFinalEstagio(String dataFinalEstagio) {
+		this.dataFinalEstagio = dataFinalEstagio;
+	}
+	public String getSeguradoraEstagio() {
+		return seguradoraEstagio;
+	}
+	public void setSeguradoraEstagio(String seguradoraEstagio) {
+		this.seguradoraEstagio = seguradoraEstagio;
+	}
+	public String getNumSiapeEstagio() {
+		return numSiapeEstagio;
+	}
+	public void setNumSiapeEstagio(String numSiapeEstagio) {
+		this.numSiapeEstagio = numSiapeEstagio;
+	}
+	public String getCargoSupervEstagio() {
+		return cargoSupervEstagio;
+	}
+	public void setCargoSupervEstagio(String cargoSupervEstagio) {
+		this.cargoSupervEstagio = cargoSupervEstagio;
+	}
+	public String getAtividadesEstagio() {
+		return atividadesEstagio;
+	}
+	public void setAtividadesEstagio(String atividadesEstagio) {
+		this.atividadesEstagio = atividadesEstagio;
+	}
+	public String getUsuarioLogin() {
+		return usuarioLogin;
+	}
+	public void setUsuarioLogin(String usuarioLogin) {
+		this.usuarioLogin = usuarioLogin;
+	}
+	public String getSenhaLogin() {
+		return senhaLogin;
+	}
+	public void setSenhaLogin(String senhaLogin) {
+		this.senhaLogin = senhaLogin;
+	}
 }
+
